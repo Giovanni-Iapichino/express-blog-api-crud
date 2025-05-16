6e5ef1b6e9dc344cad0e30e97d7aad661ae5109e
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-05-16 14:42:30 +0200

    add-middleware-notfound

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const postsRouter = require("./routers/posts");
+const postNotFound = require("./middlewares/notFound");
 
 const { posts } = require("../express-blog-api-crud/data/db");
 
 app.use(express.static("public"));
 app.use(express.json());
+app.use(postNotFound);
 
 app.use("/posts", postsRouter);
 
