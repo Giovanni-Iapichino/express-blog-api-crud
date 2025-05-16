36eff8379685209fcbbb7b1cb5236a5283c0c4d0
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-05-16 14:56:25 +0200

    add-errors-handler

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const app = express();
 const port = 3000;
 const postsRouter = require("./routers/posts");
 const postNotFound = require("./middlewares/notFound");
+const postErrorsHandler = require("./middlewares/errorsHandler");
 
 const { posts } = require("../express-blog-api-crud/data/db");
 
 app.use(express.static("public"));
 app.use(express.json());
-app.use(postNotFound);
 
 app.use("/posts", postsRouter);
+app.use(postNotFound);
+app.use(postErrorsHandler);
 
 app.listen(port, () => {
   console.log(`Server attivo su http://localhost: ` + port);
